14efb2e36fc682b9f7a97b74f1fe7c73b1eb3a0d
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-25 17:51:30 +0700

    add new method getNextState

--- Resi/src/elements/Element.java
+++ Resi/src/elements/Element.java
@@ -1,5 +1,15 @@
 package elements;
 
+import events.Event;
+import states.State;
+
 public abstract class Element {
 	public int id;
+	public Event event;
+	public State state;
+	
+	public void getNextState()
+	{
+		
+	}
 }
