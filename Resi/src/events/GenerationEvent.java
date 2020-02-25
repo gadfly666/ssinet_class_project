5e052743c8d605e8f2af90ee0ad1a0f6ee966b1a
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-25 18:53:11 +0700

    implement execute method in GenerationEvent class (event A)

--- Resi/src/events/GenerationEvent.java
+++ Resi/src/events/GenerationEvent.java
@@ -1,5 +1,23 @@
 package events;
 
+import elements.Element;
+import network.host.*;
+import network.Packet;
+import states.packet.*;
+
 public class GenerationEvent extends Event {
 	//Event dai dien cho su kien loai (A): goi tin duoc sinh ra
+	public int numSent;
+	@Override
+	
+	public void execute(Element elem)
+	{
+		if(elem instanceof SourceQueue)
+		{
+			SourceQueue sq = (SourceQueue)elem;
+			Packet p = sq.dequeue(this.startTime);
+			p.setId(numSent);
+			p.state = new StateP1();
+		}
+	}
 }
