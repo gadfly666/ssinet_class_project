4d59eabe0b2e1feed9ed77cf68284f3c9e2e42f3
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-26 12:12:59 +0700

    new body for act of Sq1 as well as new imports

--- Resi/src/states/sourcequeue/Sq1.java
+++ Resi/src/states/sourcequeue/Sq1.java
@@ -1,7 +1,26 @@
 package states.sourcequeue;
 
+import events.Event;
+import events.GenerationEvent;
+import network.host.SourceQueue;
 import states.State;
 
 public class Sq1 extends State {
 	//•	State Sq1: source queue is empty.
+	public Sq1(SourceQueue e)
+	{
+		this.elem = e;
+	}
+	
+	public void act()
+	{
+		SourceQueue sQueue = (SourceQueue)elem;
+		if(sQueue.allEvents.isEmpty())
+		{
+			Event e = new GenerationEvent();
+			e.startTime = (long)sQueue.getNextPacketTime();
+			e.endTime = e.startTime;
+			sQueue.allEvents.add(e);
+		}
+	}
 }
