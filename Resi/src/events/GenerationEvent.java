6e19bd500981e70850d2237cd4bc3679413b2d11
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-26 17:41:54 +0700

    some new statements for act( ) method

--- Resi/src/events/GenerationEvent.java
+++ Resi/src/events/GenerationEvent.java
@@ -4,6 +4,8 @@ import elements.Element;
 import network.host.*;
 import network.Packet;
 import states.packet.*;
+import states.sourcequeue.Sq1;
+import states.sourcequeue.Sq2;
 
 public class GenerationEvent extends Event {
 	//Event dai dien cho su kien loai (A): goi tin duoc sinh ra
@@ -18,7 +20,12 @@ public class GenerationEvent extends Event {
 			Packet p = sq.dequeue(this.startTime);
 			if(p == null) return;
 			p.setId(numSent);
-			p.state = new StateP1(sq);
+			p.state = new StateP1(sq, p);
+			if(sq.state instanceof Sq1)
+			{
+				sq.state = new Sq2(sq);
+				sq.state.act();
+			}
 		}
 	}
 }
