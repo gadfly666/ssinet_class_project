2b60abb29fcbaec3b8827f33329229a44a4ee100
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-26 17:55:32 +0700

    implement method: insertEvent

--- Resi/src/elements/Buffer.java
+++ Resi/src/elements/Buffer.java
@@ -9,6 +9,24 @@ public class Buffer extends Element {
 	
 	public void insertEvents(Event ev)
 	{
+		long endTime = ev.endTime;
+		int i;
+		if(allEvents == null)
+		{
+			allEvents = new ArrayList<Event>();
+			allEvents.add(ev);
+			return;
+		}
+		boolean found = false;
+		for(i = 0; i < allEvents.size() && !found; i++ )
+		{
+			if(allEvents.get(i).endTime > endTime)
+			{
+				found = true;
+				break;
+			}
+		}
+		allEvents.add(i, ev);
 		
 	}
 }
