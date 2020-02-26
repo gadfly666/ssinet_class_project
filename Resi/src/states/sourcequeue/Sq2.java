4ed1f69944be1e62733a198df190be55e2a39049
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-26 12:13:48 +0700

    change superclass of Sq2 in order to re-use code

--- Resi/src/states/sourcequeue/Sq2.java
+++ Resi/src/states/sourcequeue/Sq2.java
@@ -1,7 +1,14 @@
 package states.sourcequeue;
 
-import states.State;
 
-public class Sq2 extends State {
+import network.host.SourceQueue;
+
+public class Sq2 extends Sq1 {
 	//•	State Sq2: source queue is not empty.
+	public Sq2(SourceQueue e)
+	{
+		super(e);
+	}
+	
+	
 }
