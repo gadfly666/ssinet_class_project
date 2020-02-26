f44109fcd213a8c9ab8e3e18251b338bc82e3b61
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-26 10:55:20 +0700

    add new constructor StateP1(Element e)

--- Resi/src/states/packet/StateP1.java
+++ Resi/src/states/packet/StateP1.java
@@ -1,7 +1,13 @@
 package states.packet;
 
 import states.State;
+import elements.*;
 
 public class StateP1 extends State {
 	//•	State P1: the packet is generated
+	
+	public StateP1(Element e)
+	{
+		this.elem = e;
+	}
 }
