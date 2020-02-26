bbef43d29a5b0ca5c2743fd9f00535e9068064ca
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-26 17:44:26 +0700

    new attribute, new constructor and new body for act( ) method

--- Resi/src/states/packet/StateP1.java
+++ Resi/src/states/packet/StateP1.java
@@ -1,18 +1,39 @@
 package states.packet;
 
 import states.State;
-import elements.*;
+import network.Packet;
+import network.host.SourceQueue;
 
 public class StateP1 extends State {
 	//•	State P1: the packet is generated
+	public Packet p;
 	
-	public StateP1(Element e)
+	public StateP1(SourceQueue e)
 	{
 		this.elem = e;
 	}
 	
+	public StateP1(SourceQueue e, Packet p)
+	{
+		this.elem = e;
+		this.p = p;
+	}
+	
 	public void act()
 	{
-		
+		SourceQueue sQueue = (SourceQueue)elem;
+		if(sQueue == null || this.p == null)
+		{
+			return;
+		}
+		if(sQueue.allPackets == null)
+		{
+			System.out.println("Empty packet in source queue error!\n");
+			return;
+		}
+		if(sQueue.allPackets.get(0) == this.p)
+		{
+			
+		}
 	}
 }
