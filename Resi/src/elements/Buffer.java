102c1bf0e9e87cdcd0e3c9b7fbd0188323cc559f
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-26 17:32:20 +0700

    add new property: allEvents and then all SourceQueue, ENB, EXB will inherit

--- Resi/src/elements/Buffer.java
+++ Resi/src/elements/Buffer.java
@@ -1,5 +1,14 @@
 package elements;
 
-public class Buffer extends Element {
+import java.util.ArrayList;
 
+import events.Event;
+
+public class Buffer extends Element {
+	public ArrayList<Event> allEvents = new ArrayList<Event>();
+	
+	public void insertEvents(Event ev)
+	{
+		
+	}
 }
