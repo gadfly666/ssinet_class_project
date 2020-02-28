12be98b3fd764d77b239167c85c69a966fde404f
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-28 09:58:57 +0700

    add new Constructor PhysicalLayer(Host host)

--- Resi/src/network/layers/PhysicalLayer.java
+++ Resi/src/network/layers/PhysicalLayer.java
@@ -7,4 +7,14 @@ public class PhysicalLayer {
 	public ExitBuffer[] EXBs;
 	public EntranceBuffer[] ENBs;
 	public SourceQueue sq;
+	
+	public PhysicalLayer(Host host)
+	{
+		ENBs = null;
+		EXBs = new ExitBuffer[0];
+		sq = new SourceQueue(host.id);
+		EXBs[0] = new ExitBuffer();
+		EXBs[0].phyLayer = this;
+		sq.phyLayer = this;
+	}
 }
