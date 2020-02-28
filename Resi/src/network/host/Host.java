43b1ef13dad8528dd7e205be5c045f853dd2f322
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-28 18:38:26 +0700

    edit methods Host(int id) and generatePacket(int destination)

--- Resi/src/network/host/Host.java
+++ Resi/src/network/host/Host.java
@@ -1,8 +1,11 @@
 package network.host;
 
 import config.Constant;
+import events.Event;
+import events.GenerationEvent;
 import network.Link;
 import network.Node;
+import network.layers.PhysicalLayer;
 
 
 
@@ -11,16 +14,24 @@ import network.Node;
  */
 public class Host extends Node {
 	public Host(int id) {
-	        super(id);
+	     super(id);
+	     this.physicalLayer = new PhysicalLayer(this);
 	}
    @Override
     public void clear() {
 
     }
    
-   public void generate( ) 
+   public void generatePacket(int destination) 
    {
-	   
+	   if(this.physicalLayer.sq == null)
+		   this.physicalLayer.sq = new SourceQueue(this.id, destination);
+	   else
+		   this.physicalLayer.sq.setDestionationID(destination);
+	   Event ev = new GenerationEvent(this.physicalLayer.sq);
+	   ev.startTime = this.physicalLayer.sim.time();
+	   ev.endTime = ev.startTime;
+	   this.physicalLayer.sq.insertEvents(ev);
    }
    
    public void receive()
