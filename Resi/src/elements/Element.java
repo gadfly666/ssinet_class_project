51b396906db81789f06b9134d794a7a6e422b25a
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-02-28 09:46:30 +0700

    remove attribute event

--- Resi/src/elements/Element.java
+++ Resi/src/elements/Element.java
@@ -1,11 +1,9 @@
 package elements;
 
-import events.Event;
 import states.State;
 
 public abstract class Element {
 	public int id;
-	//public Event event;
 	public State state;
 	
 	public void getNextState()
