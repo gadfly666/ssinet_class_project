b146c301862e62b4e15c48dceaa56f5a2a1cf145
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-03-02 11:04:28 +0700

    add new property and new method

--- Resi/src/elements/Element.java
+++ Resi/src/elements/Element.java
@@ -5,8 +5,14 @@ import states.State;
 public abstract class Element {
 	public int id;
 	public State state;
+	public long soonestEndTime = 0;
 	
 	public void getNextState()
+	{
+		
+	}
+	
+	public void updateSoonestEndTime()
 	{
 		
 	}
