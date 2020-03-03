cd355a47e2806cd17246abf6cd78544bd8adcca0
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-03-03 18:24:11 +0700

    prepare for override method execute

--- Resi/src/events/LeavingSourceQueueEvent.java
+++ Resi/src/events/LeavingSourceQueueEvent.java
@@ -1,5 +1,7 @@
 package events;
 
+import network.host.SourceQueue;
+
 enum TypeB
 {
 	B, B1, B2, B3, B4
@@ -8,4 +10,11 @@ enum TypeB
 public class LeavingSourceQueueEvent extends Event {
 	public TypeB type = TypeB.B;
 	//Event dai dien cho su kien loai (B): goi tin roi khoi Source Queue
+	public void execute()
+	{
+		if(elem instanceof SourceQueue)
+		{
+			
+		}
+	}
 }
