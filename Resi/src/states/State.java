806b63e34d0ecd9db7e93fa5371d9447dd7704a6
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-03-03 18:02:51 +0700

    new attribute ancestorEvent

--- Resi/src/states/State.java
+++ Resi/src/states/State.java
@@ -4,6 +4,7 @@ import elements.Element;
 import events.Event;
 
 public abstract class State {
+	public Event ancestorEvent;
 	public Element elem;
 	public void act() {}
 	public void getNextState(Element e) {}
