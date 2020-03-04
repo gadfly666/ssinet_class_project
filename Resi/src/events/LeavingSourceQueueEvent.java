c8ae7a316bb3685ddf2be369bed4e6319f0d2b8a
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-03-04 09:33:29 +0700

    new constructor and continue to modify execute method

--- Resi/src/events/LeavingSourceQueueEvent.java
+++ Resi/src/events/LeavingSourceQueueEvent.java
@@ -1,7 +1,11 @@
 package events;
 
+import config.Constant;
 import elements.Element;
+import elements.ExitBuffer;
+import network.Packet;
 import network.host.SourceQueue;
+import states.sourcequeue.Sq2;
 
 enum TypeB
 {
@@ -12,15 +16,31 @@ public class LeavingSourceQueueEvent extends Event {
 	public TypeB type = TypeB.B;
 	//Event dai dien cho su kien loai (B): goi tin roi khoi Source Queue
 	
-	public LeavingSourceQueueEvent(Element elem)
+	public LeavingSourceQueueEvent(Element elem, Packet p)
 	{
 		this.elem = elem;
+		this.pid = p.id;
 	}
 	
 	@Override
 	public void execute()
 	{
 		elem.removeExecutedEvent(this);
-		
+		SourceQueue sQueue = (SourceQueue)elem;
+		ExitBuffer exb = sQueue.phyLayer.EXBs[0];//Kiem tra xem EXB co cho trong hay khong?
+		int index = exb.indexOfEmpty();
+		if(index < Constant.QUEUE_SIZE
+				&& (sQueue.state instanceof Sq2)
+				)//neu EXB con cho trong
+		{
+			Packet p = sQueue.allPackets.remove(0);
+			exb.insertPacket(p);
+			//To be continued...
+			//Event e = new LeavingSourceQueueEvent(sQueue);
+			//e.startTime = sQueue.phyLayer.sim.time();
+			//e.endTime = e.startTime;
+			//e.pid = this.p.id;
+			//sQueue.insertEvents(e);//chen them su kien moi vao
+		}
 	}
 }
