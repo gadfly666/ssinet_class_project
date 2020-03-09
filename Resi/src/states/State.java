b5518c5b5322b20ecdcfc2a6c00ef3d69cc7a630
thanhnt812 <nguyenthanh@soict.hust.edu.vn>
2020-03-09 10:45:47 +0700

    change act( ) to some overload act methods

--- Resi/src/states/State.java
+++ Resi/src/states/State.java
@@ -1,12 +1,20 @@
 package states;
 
 import elements.Element;
-import events.Event;
+import events.*;
 
 public abstract class State {
 	public Event ancestorEvent;
 	public Element elem;
-	public void act() {}
+	public void act(GenerationEvent ev) {}
+	public void act(LeavingEXBEvent ev) {}
+	public void act(LeavingSourceQueueEvent ev) {}
+	public void act(LeavingSwitchEvent ev) {}
+	public void act(MovingInSwitchEvent ev) {}
+	public void act(NotificationEvent ev) {}
+	public void act(ReachingDestinationEvent ev) {}
+	public void act(ReachingENBEvent ev) {}
+	
 	public void getNextState(Element e) {}
 	
 	
